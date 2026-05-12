c79f53e8e468d1d70925c979f63d0a6d420d8c22
Hao Zhao <zhaohao1202@outlook.com>
2026-05-12 15:56:17 +0200

    Update checkpoint paths

--- shscripts/inference_hunyuanvideo_t2v.sh
+++ shscripts/inference_hunyuanvideo_t2v.sh
@@ -2,7 +2,7 @@ config='configs/007_hunyuanvideo/hunyuanvideo_t2v_diffuser.yaml'
 prompt_file="inputs/t2v/hunyuanvideo/tyler_swift_video/labels.txt"
 current_time=$(date +%Y%m%d%H%M%S)
 savedir="results/t2v/$current_time-hunyuanvideo"
-ckpt="checkpoints/hunyuanvideo/HunyuanVideo"
+ckpt="/work/nlp/hzhao/ttt-imagine/checkpoints/hunyuanvideo/HunyuanVideo"
 
 python3 scripts/inference_cogvideo.py \
 --ckpt_path $ckpt \
