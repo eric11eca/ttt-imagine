4427a89b197c9d61930c451de6aeb33500db1ce5
Hao Zhao <zhaohao1202@outlook.com>
2026-05-13 09:52:38 +0200

    Update checkpoint path

--- shscripts/train_wanvideo_t2v_e2e_ttt_lora.sh
+++ shscripts/train_wanvideo_t2v_e2e_ttt_lora.sh
@@ -1,7 +1,7 @@
 export TOKENIZERS_PARALLELISM=false
 
 # Base checkpoint directory (Wan2.1-T2V-14B)
-CKPT="checkpoints/wan/Wan2.1-T2V-14B"
+CKPT="/work/nlp/hzhao/ttt-imagine/checkpoints/wan/Wan2.1-T2V-14B"
 
 # E2E-TTT config (VideoTuna schema). Once the E2E-TTT flow exists, this will launch meta-training.
 CONFIG="configs/008_wanvideo/wan2_1_t2v_14B_e2e_ttt_lora.yaml"
